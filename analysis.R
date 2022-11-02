b2d57e27507810567d1f9e47bc11def444ee4b9b
LeoZotos <zotos.leo@gmail.com>
2022-11-02 14:00:04 +0100

    added anova test and linear effects model. Not enough data points for anything useful

--- analysis.R
+++ analysis.R
@@ -1,17 +1,41 @@
+library(dplyr)
+library(lme4)
+
 dat <- read.csv(file.choose())
 
-
-nrow(subset(dat, correct=='True'))
-nrow(subset(dat, correct=='False'))
-
-# Amount of fact introduced
-unique(dat$fact_id)
+# Number of fact introduced
+# unique(dat$fact_id)
 
 #Threshold for items to be considered as learned
 threshold = 0.5
 nrow(subset(dat, alpha>threshold))
 learned <- subset(dat, alpha>threshold)
 
-#Amount of learned facts
-length(unique(learned$fact_id))
+#Keep unique values (each fact is only counts as learned once)
+learned = distinct(learned, subject,question, .keep_all= TRUE)
+
+
+#Number of learned facts, per subject
+learnedPerSubject <- learned %>% group_by(condition, subject) %>% 
+  summarise(countLearned=n(),.groups = 'drop') %>% 
+  as.data.frame()
+
+
+###Statistical Analysis
+learnedPerSubject$condition <- as.factor(learnedPerSubject$condition)
+learnedPerSubject$subject <- as.factor(learnedPerSubject$subject)
+
+##Simple ANOVA
+anovaModel <- aov(countLearned ~ condition + subject, data = learnedPerSubject)
+summary(anovaModel)
+
+
+## Creation of two linear mixed effects models
+linearModel1 <- lmer(countLearned ~ condition + (1|condition), data=learnedPerSubject, REML=FALSE)
+linearModel2 <- lmer(countLearned ~ (1|condition), data=learnedPerSubject, REML=FALSE)
+
+anova(linearModel1, linearModel2)
+
+summary(linearModel2)
+
 
